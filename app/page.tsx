76f65c4212bf707f83938ac84b8d53a59543d597
Austinobravo <Austinobravo@gmail.com>
2024-07-16 00:50:05 +0100

    finishing touch

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ const Home = () => {
 
   return (
     <>
-    <section>
+    <section className="pb-20">
     <nav className='bg-white w-full !py-7 !px-8 shadow-md fixed'>
         <Link href={`/`} className=' '>
             <Image src={`/logo-no-background.svg`} alt='' width={100} height={100} className=''/>
