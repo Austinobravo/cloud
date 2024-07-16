84418add3ca1355d299b75d613c5bf235fb234db
Austinobravo <Austinobravo@gmail.com>
2024-07-16 01:18:40 +0100

    Added error logging in the backend

--- app/api/convert/route.ts
+++ app/api/convert/route.ts
@@ -5,12 +5,16 @@ export async function POST(params:Request) {
     const data = await params.json()
     const fileName = data.title ? `${data.title}.txt` : 'cloudimages.txt'
     const filePath = path.join(process.cwd(),'public/files', fileName)
-    for(let file of data.files){
-        fs.appendFileSync(filePath, file+"\n")
+    try{
+        for(let file of data.files){
+            fs.appendFileSync(filePath, file+"\n")
+        }
+    
+        const fileUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/files/${fileName}`
+    
+        return  NextResponse.json( {message:'Gotten', url: fileUrl})
+    }catch(error){
+        return NextResponse.json(error)
     }
-
-    const fileUrl = `${process.env.NEXT_PUBLIC_BASE_URL}/files/${fileName}`
-
-    return  NextResponse.json( {message:'Gotten', url: fileUrl})
     
 }
